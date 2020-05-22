7239daeeebcdefd8e7bb9cd120a8af28c035248c
kenny5660 <misha25_live@mail.ru>
2020-05-22 05:52:22 -0400

    add lab 9

---
+++ lab_9/hw/main-race-good.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "mythreads.h"
+#include <stdatomic.h>
+
+atomic_int balance  = 0;
+
+void* worker(void* arg) {
+    balance++; // unprotected access 
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t p;
+    Pthread_create(&p, NULL, worker, NULL);
+    balance++; // unprotected access
+    Pthread_join(p, NULL);
+    return 0;
+}
